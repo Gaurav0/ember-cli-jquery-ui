47bf9b8a48c3bf3464b7f156a83e3a5dbfcc1d72
Gaurav Munjal <Gaurav0@aol.com>
2014-10-07 01:15:43 -0700

    Add jquery-ui package

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = {
-  name: 'ember-cli-jquery-ui'
+  name: 'ember-cli-jquery-ui',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('bower_components/jquery-ui/jquery-ui.js');
+    app.import('bower_components/jquery-ui/themes/base/all.css')
+  }
 };
