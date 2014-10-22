fe5da4438551a788c021ae487b3daa5928b6fa8f
Gaurav Munjal <Gaurav0@aol.com>
2014-10-22 15:41:51 -0700

    Fixes to slider widget

--- addon/components/jqui-slider/component.js
+++ addon/components/jqui-slider/component.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 import jquiWidget from 'ember-cli-jquery-ui/mixins/jqui-widget';
 
-export default Ember.TextField.extend(jquiWidget, {
+export default Ember.Component.extend(jquiWidget, {
     uiType: 'slider',
     uiOptions: ['animate', 'disabled', 'max', 'min', 'orientation', 'range', 'step', 'value', 'values'  ],
-    uiEvents: ['change', 'create', 'slide', 'start', 'stop']
+    uiEvents: ['change', 'create', 'slide', 'start', 'stop'],
+    slide: function(event, ui) {
+        this.set('value', ui.value);
+    },
+    change: function(event, ui) {
+        var _this = this;
+        Ember.run.next(function() {
+            _this.set('value', ui.value);
+        });
+    }
 });
