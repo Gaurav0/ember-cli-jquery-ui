97cbc5b0cf8005e5fa8275a876f29f7ff7484581
Gaurav Munjal <Gaurav0@aol.com>
2015-04-07 14:56:55 -0400

    Add test for gatherOptions in JquiWidgetMixin

--- tests/unit/mixins/jqui-widget-test.js
+++ tests/unit/mixins/jqui-widget-test.js
@@ -3,9 +3,18 @@ import JquiWidgetMixin from 'ember-cli-jquery-ui/mixins/jqui-widget';
 
 module('JquiWidgetMixin');
 
-// Replace this with your real tests.
 test('it works', function() {
   var JquiWidgetObject = Ember.Object.extend(JquiWidgetMixin);
   var subject = JquiWidgetObject.create();
   ok(subject);
+});
+
+test('gathers options', function() {
+  var JquiWidgetObject = Ember.Object.extend(JquiWidgetMixin, {
+    uiOptions: ['option1', 'option2']
+  });
+  var subject = JquiWidgetObject.create();
+  var options = subject._gatherOptions();
+  ok('option1' in options);
+  ok('option2' in options);
 });
